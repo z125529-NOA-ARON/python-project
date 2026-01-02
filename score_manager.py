f04c09d5ef20dd94a07558ca73e2d194b7c4b32e
z125470-Eunice-Merce <z125470@shibaura-it.ac.jp>
2026-01-02 20:24:08 +0900

    Delete score_manager.py

--- score_manager.py
+++
@@ -1,9 +0,0 @@
-class ScoreManager:
-    def __init__(self, user_manager):
-        self.user_manager = user_manager
-
-    def update_score(self, username, score):
-        self.user_manager.update_best_score(username, score)
-
-    def get_top_4(self):
-        return self.user_manager.get_top_4()
