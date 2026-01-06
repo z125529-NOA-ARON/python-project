f75426697ea371faca107f1bd3933509843d2dfb
z125470-Eunice-Merce <z125470@shibaura-it.ac.jp>
2026-01-06 17:26:41 +0900

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -3,23 +3,28 @@ import random
 import personalisation
 import os
 
+# screen size 
 screen_width = 800
 screen_height = 400
 
-
-
-
 class SnakeGame:
+    """
+    SnakeGame: Eunice - z125470
+    handling the Snake game logic
+    Manages the game loop, snake movement, collisions, score handling, and rendering using pygame
+    Interacts with ScoreManager to save scores and displays the game over screen when the player loses
+    """
+
     def __init__(self, username, score_manager):
-        self.username = username                         # store the player's username
-        self.score_manager = score_manager               # store the score manager object
+        self.username = username # store the player's username
+        self.score_manager = score_manager # store the score manager object
 
         # Window + block size
         self.width = screen_width
         self.height = screen_height
         self.block = 20
 
-        # download images --> use images from same folder as this file
+         # download images --> use images from same folder as this file
         base_dir = os.path.dirname(os.path.abspath(__file__))
         self.grass = pygame.image.load(os.path.join(base_dir, "grass.png"))
         self.grass = pygame.transform.scale(self.grass, (self.width, self.height))
@@ -32,34 +37,34 @@ class SnakeGame:
 
     # fonction of the reset
     def reset_game(self):
-        self.snake = [(self.width/2, self.height/2)]          # initialise the snake at the position (100,100); the size is 1 segment 
-        self.direction = (self.block, 0)                      # initialise the direction of the snake to the right
-        self.score = 0                                        # reset the score to 0
-        self.food = self.spawn_food()                         # generate the first food position
-        self.is_running = True                                # the game running varable is True
+        self.snake = [(self.width/2, self.height/2)]  # initialise the snake at the position (100,100); the size is 1 segment 
+        self.direction = (self.block, 0) # initialise the direction of the snake to the right
+        self.score = 0 # reset the score to 0
+        self.food = self.spawn_food() # generate the first food position
+        self.is_running = True # the game running varable is True
 
     # function to make food appear
     def spawn_food(self):
         return (random.randint(0, (self.width - self.block) // self.block) * self.block, random.randint(0, (self.height - self.block) // self.block) * self.block) # random position (x, y)
 
-    # 
+    # function to move the snake
     def move_snake(self):
-        x, y = self.snake[0]               # get the position of the snake
-        dx, dy = self.direction            # get the movement direction
-        new_head = (x + dx, y + dy)        # calculate the position of the new head
-        self.snake.insert(0, new_head)     # add the new head at the beginning of the snake list 
+        x, y = self.snake[0] # get the position of the snake
+        dx, dy = self.direction # get the movement direction
+        new_head = (x + dx, y + dy) # calculate the position of the new head
+        self.snake.insert(0, new_head) # add the new head at the beginning of the snake list 
 
         # check if the snake ate an appel
         if new_head == self.food:      
-            self.score += 1                # increase the score of 1
-            self.food = self.spawn_food()  # respawn an apple 
+            self.score += 1 # increase the score of 1
+            self.food = self.spawn_food() # respawn an apple 
         else:
-            self.snake.pop()               # remove the last segment of the snake to have the good number of segment
+            self.snake.pop() # remove the last segment of the snake to have the good number of segment
 
 
     # Check if the snake touch itself or the wall
     def check_collisions(self):
-        x, y = self.snake[0]               # get the position of the head of the snake
+        x, y = self.snake[0] # get the position of the head of the snake
 
         # if the snake touch the wall 
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
@@ -69,20 +74,21 @@ class SnakeGame:
         if self.snake[0] in self.snake[1:]:
             return True
 
-        return False                        # no collision
+        return False # no collision
 
     # fonction to initialise the start
     def start(self):
         pygame.init()
-        screen = pygame.display.set_mode((self.width, self.height))    # create the game window
-        clock = pygame.time.Clock()                                    # creation of the clock to control the FPS
+        screen = pygame.display.set_mode((self.width, self.height)) # create the game window
+        clock = pygame.time.Clock() # creation of the clock to control the FPS
 
-        from game_over import game_over_screen                         # import game over screen fonction
-        from menu import menu                                          # import menu fonction
+        from game_over import game_over_screen # import game over screen fonction
+        # from menu import menu  # import menu fonction
 
-        font = pygame.font.SysFont("Comic Sans MS", 20)                        # creation of the font for the score display
+        font = pygame.font.SysFont("Comic Sans MS", 20) # creation of the font for the score display
 
         while True:
+            # handle the event we do 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -108,21 +114,20 @@ class SnakeGame:
                     # press escape to go up
                     elif event.key == pygame.K_ESCAPE:
                         # Retour menu principal
-                        return                           #return to the main menu
+                        return #return to the main menu
 
-            self.move_snake()                            # move the snake
+            self.move_snake() # move the snake
 
-            if self.check_collisions():                                     # chek the collision
-                self.score_manager.update_score(self.username, self.score)  # save the score
-                top4 = self.score_manager.get_top_4()                       # have the top 4
-                game_over_screen(screen, self.score, top4)                  # display the game over screen
+            if self.check_collisions(): # chek the collision
+                self.score_manager.update_score(self.username, self.score) # save the score
+                top4 = self.score_manager.get_top_4() # have the top 4
+                game_over_screen(screen, self.score, top4) # display the game over screen
                 return
 
             # Display
-            screen.blit(self.grass, (0, 0))                                 # display the background 
+            screen.blit(self.grass, (0, 0)) # display the background 
 
             # diplay the snake segments
-            
             for block in self.snake:
                 pygame.draw.rect(screen, personalisation.snake_color, (*block, self.block, self.block),border_radius =  30)
 
@@ -133,5 +138,5 @@ class SnakeGame:
             score_text = font.render(f"Score : {self.score}", True, (255, 255, 255))
             screen.blit(score_text, (self.width - 105, 5))
 
-            pygame.display.flip()                                           #update the display
-            clock.tick(10)                                                  # 10 FPS 
+            pygame.display.flip() #update the display
+            clock.tick(10) # 10 FPS 
