ebf651757de89e44675c1e0ee7d84405e8bc9aa7
z125560-Timo-Lantres <z125560@shibaura-it.ac.jp>
2026-01-07 23:07:34 +0900

    Update score_manager.py

--- score_manager.py
+++ score_manager.py
@@ -5,7 +5,8 @@ to the rest of the application.
 
 
 class ScoreManager:
-    """Adapter that forwards score operations to a `UserManager` instance.
+    """
+    Adapter that forwards score operations to a `UserManager` instance.
 
     Parameters
     - user_manager: an instance providing `update_best_score(username, score)`
@@ -19,10 +20,14 @@ class ScoreManager:
         self.user_manager = user_manager
 
     def update_score(self, username, score):
-        """Update the stored best score for `username` if `score` is higher."""
+        """
+        Update the stored best score for `username` if `score` is higher.
+        """
         self.user_manager.update_best_score(username, score)
 
     def get_top_4(self):
-        """Return the top 4 users as provided by `UserManager.get_top_4()`."""
+        """
+        Return the top 4 users as provided by `UserManager.get_top_4()`.
+        """
         return self.user_manager.get_top_4()
 
